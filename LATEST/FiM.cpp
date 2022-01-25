2e145e78d2cdcc8be652d6d50e97e08e2799f1ed
root <nagaraja.hm.0001@gmail.com>
2022-01-25 09:55:22 +0100

    Update: submodule FiM

--- LATEST/FiM.cpp
+++ LATEST/FiM.cpp
@@ -5,3 +5,33 @@ class_FiM FiM;
 FUNC(void, FIM_CODE) class_FiM::InitFunction(void){
 }
 
+FUNC(void, FIM_CODE) class_FiM::GetFunctionPermission(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::SetFunctionAvailable(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::DemTriggerOnMonitorStatus(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::DemTriggerOnComponentStatus(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::DemInitFunction(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::GetVersionInfo(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::MainFunction(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::FunctionInhibition(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::ControlFunctionAvailable(void){
+}
+
+FUNC(void, FIM_CODE) class_FiM::FunctionIdType(void){
+}
+
--- LATEST/FiM.h
+++ LATEST/FiM.h
@@ -5,7 +5,17 @@
 
 class class_FiM{
    public:
-      FUNC(void, FIM_CODE) InitFunction(void);
+      FUNC(void, FIM_CODE) InitFunction                (void);
+      FUNC(void, FIM_CODE) GetFunctionPermission       (void);
+      FUNC(void, FIM_CODE) SetFunctionAvailable        (void);
+      FUNC(void, FIM_CODE) DemTriggerOnMonitorStatus   (void);
+      FUNC(void, FIM_CODE) DemTriggerOnComponentStatus (void);
+      FUNC(void, FIM_CODE) DemInitFunction             (void);
+      FUNC(void, FIM_CODE) GetVersionInfo              (void);
+      FUNC(void, FIM_CODE) MainFunction                (void);
+      FUNC(void, FIM_CODE) FunctionInhibition          (void);
+      FUNC(void, FIM_CODE) ControlFunctionAvailable    (void);
+      FUNC(void, FIM_CODE) FunctionIdType              (void);
 };
 
 extern class_FiM FiM;
