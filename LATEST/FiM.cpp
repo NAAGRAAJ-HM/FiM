75229c53db756d8d5652786ffc6802a75259808e
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-03 10:09:15 +0000

    Update: AUTOSAR basic software service module FiM

--- LATEST/FiM.cpp
+++ LATEST/FiM.cpp
@@ -82,79 +82,79 @@ FUNC(void, FIM_CODE) module_FiM::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, FIM_CONFIG_DATA, FIM_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == FiM_InitCheck)
-   if(E_OK == IsInitDone){
-#if(STD_ON == FiM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK != IsInitDone){
 #endif
-   }
-   else{
-#endif
-      if(NULL_PTR == lptrCfgModule){
-#if(STD_ON == FiM_DevErrorDetect)
-         Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-         );
-#endif
-      }
-      else{
+      if(NULL_PTR != lptrCfgModule){
          if(STD_LOW){
-// check lptrCfgModule for memory faults
+            // check lptrCfgModule for memory faults
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfgCanIf as back-up configuration
+            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgFiM;
          }
       }
-      IsInitDone = E_OK;
+      else{
+#if(STD_ON == FiM_DevErrorDetect)
+         Det_ReportError(
+               0 //TBD: IdModule
+            ,  0 //TBD: IdInstance
+            ,  0 //TBD: IdApi
+            ,  0 //TBD: IdError
+         );
+#endif
+      }
 #if(STD_ON == FiM_InitCheck)
+      IsInitDone = E_OK;
+   }
+   else{
+#if(STD_ON == FiM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
 
 FUNC(void, FIM_CODE) module_FiM::DeInitFunction(void){
 #if(STD_ON == FiM_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == FiM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
+   if(E_OK == IsInitDone){
 #endif
+#if(STD_ON == FiM_InitCheck)
+      IsInitDone = E_NOT_OK;
    }
    else{
+#if(STD_ON == FiM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
 #endif
-      IsInitDone = E_NOT_OK;
-#if(STD_ON == FiM_InitCheck)
    }
 #endif
 }
 
 FUNC(void, FIM_CODE) module_FiM::MainFunction(void){
 #if(STD_ON == FiM_InitCheck)
-   if(E_OK != IsInitDone){
-#if(STD_ON == FiM_DevErrorDetect)
-      Det_ReportError(
-      0 //TBD: IdModule
-   ,  0 //TBD: IdInstance
-   ,  0 //TBD: IdApi
-   ,  0 //TBD: IdError
-      );
-#endif
-   }
-   else{
+   if(E_OK == IsInitDone){
 #endif
 #if(STD_ON == FiM_InitCheck)
+   }
+   else{
+#if(STD_ON == FiM_DevErrorDetect)
+      Det_ReportError(
+            0 //TBD: IdModule
+         ,  0 //TBD: IdInstance
+         ,  0 //TBD: IdApi
+         ,  0 //TBD: IdError
+      );
+#endif
    }
 #endif
 }
