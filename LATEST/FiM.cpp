854dafd0a6d20fe3014e39a1ee3b23682368b525
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:36:39 +0000

    Update: AUTOSAR basic software service module FiM

--- LATEST/FiM.cpp
+++ LATEST/FiM.cpp
@@ -35,6 +35,13 @@ class module_FiM:
       public abstract_module
    ,  public class_FiM_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_FiM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
