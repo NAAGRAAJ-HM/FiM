b1b2270f0b7938dc80f94f0cb370d28e29d1a914
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-11 10:15:37 +0000

    Update: AUTOSAR basic software service module FiM

--- LATEST/FiM.cpp
+++ LATEST/FiM.cpp
@@ -9,7 +9,7 @@
 #include "Module.hpp"
 #include "CfgFiM.hpp"
 #include "FiM_core.hpp"
-#include "infFiM.hpp"
+#include "infFiM_Exp.hpp"
 
 /******************************************************************************/
 /* #DEFINES                                                                   */
@@ -32,6 +32,7 @@
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
 class module_FiM:
+   INTERFACES_EXPORTED_FIM
       public abstract_module
    ,  public class_FiM_Functionality
 {
