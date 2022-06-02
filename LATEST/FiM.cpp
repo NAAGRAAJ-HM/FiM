e97c0cb6c084e7fd88a46c1878480900ad4d7f12
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-02 12:07:25 +0200

    Update: AUTOSAR service submodule - FiM

--- LATEST/FiM.cpp
+++ LATEST/FiM.cpp
@@ -72,6 +72,10 @@ CONSTP2VAR(infSchMClient, FIM_VAR, FIM_CONST) gptrinfSchMClient_FiM = &FiM;
 /******************************************************************************/
 VAR(module_FiM, FIM_VAR) FiM(
    {
+#if(STD_ON == _ReSIM)
+// char strModuleName[6];
+#else
+#endif
          FIM_AR_RELEASE_VERSION_MAJOR
       ,  FIM_AR_RELEASE_VERSION_MINOR
       ,  0x00
