df589c1ced63e8ce77b00edc9ed9301770e99fd9
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:50:35 +0100

    Update: AUTOSAR basic software service module for function inhibition manager

---
+++ main_R2023/infServiceFiM_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceFiM_ServiceNvM.hpp                                      */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICEFIM_AR_RELEASE_VERSION_MAJOR                               4
+#define CONSTSERVICEFIM_AR_RELEASE_VERSION_MINOR                               3
+
+#define Const_DefaultInit_ConstServiceFiM                                                                              \
+   ,  {CONSTSERVICEFIM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICEFIM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
