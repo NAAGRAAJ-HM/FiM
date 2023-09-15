109f831dc728361f051e2f613162dc70904743cb
Raajnaag Huliyapurada Mata <nagaraja.hm.0001@gmail.com>
2023-09-15 15:52:02 +0200

    debug CanTp multi-frame issue

---
+++ pRealEcu_R2023.09.01/api/FiM_Types.hpp
@@ -0,0 +1,12 @@
+#ifndef FIM_TYPES_H
+#define FIM_TYPES_H
+
+#include "Std_Types.hpp"
+#include "Rte_FiM_Type.hpp"
+
+typedef uint8 FiM_InhibitionMaskType;
+typedef uint32 FiM_NumOffsetEventType;
+typedef uint32 FiM_NumOffsetComponentType;
+
+#endif
+
