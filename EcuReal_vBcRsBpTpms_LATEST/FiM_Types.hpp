6a36e3a7909b0f6c88bea125cdb7b28f74acf014
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2023-11-20 08:22:11 +0100

    AUTOSAR Migration Activities - EcuReal_vBcRsBpTpms_LATEST - SwcServiceFiM

---
+++ EcuReal_vBcRsBpTpms_LATEST/FiM_Types.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "types.hpp"
+
+typedef uint8  FiM_FunctionIdType;
+typedef uint8  FiM_InhibitionMaskType;
+typedef uint32 FiM_NumOffsetEventType;
+typedef uint32 FiM_NumOffsetComponentType;
